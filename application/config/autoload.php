e5ab1c7ed630d58cce17de8cdd4a0b2819c45d01
Ryo <rics.castillo3@gmail.com>
2024-11-27 01:30:30 +0800

    second

---
+++ application/config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+$autoload['libraries'] =  array('database', 'session');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url', 'session','system_template', 'utils', 'message', 'encryption', 'login_template','security');
+$autoload['config'] = array('');
+$autoload['language'] = array();
+$autoload['model'] = array();
